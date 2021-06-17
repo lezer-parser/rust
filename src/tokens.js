b88453b55835980d46c7122aaf9c7a4151b96df2
Marijn Haverbeke <marijn@haverbeke.nl>
2021-06-17 17:48:48 +0200

    Port tokenizers to new InputStream interface

--- src/tokens.js
+++ src/tokens.js
@@ -1,70 +1,72 @@
 import {ExternalTokenizer} from "lezer"
 import {Float, RawString, closureParamDelim, tpOpen, tpClose} from "./parser.terms"
 
-const _b = 98, _e = 101, _f = 102, _r = 114, _E = 69,
-  Dot = 46, Plus = 43, Minus = 45, Hash = 35, Quote = 34, Pipe = 124, LessThan = 60, GreaterThan = 62
+const _b = 98, _e = 101, _f = 102, _r = 114, _E = 69, Zero = 48,
+      Dot = 46, Plus = 43, Minus = 45, Hash = 35, Quote = 34, Pipe = 124, LessThan = 60, GreaterThan = 62
 
 function isNum(ch) { return ch >= 48 && ch <= 57 }
 function isNum_(ch) { return isNum(ch) || ch == 95 }
 
-export const literalTokens = new ExternalTokenizer((input, token, stack) => {
-  let pos = token.start, next = input.get(pos)
-  if (isNum(next)) {
+export const literalTokens = new ExternalTokenizer((input, stack) => {
+  if (isNum(input.next)) {
     let isFloat = false
-    do { next = input.get(++pos) } while (isNum_(next))
-    if (next == Dot) {
+    do { input.advance() } while (isNum_(input.next))
+    if (input.next == Dot) {
       isFloat = true
-      next = input.get(++pos)
-      if (isNum(next)) {
-        do { next = input.get(++pos) } while (isNum_(next))
-      } else if (next == Dot || next > 0x7f || /\w/.test(String.fromCharCode(next))) {
+      input.advance()
+      if (isNum(input.next)) {
+        do { input.advance() } while (isNum_(input.next))
+      } else if (input.next == Dot || input.next > 0x7f || /\w/.test(String.fromCharCode(input.next))) {
         return
       }
     }
-    if (next == _e || next == _E) {
+    if (input.next == _e || input.next == _E) {
       isFloat = true
-      next = input.get(++pos)
-      if (next == Plus || next == Minus) next = input.get(++pos)
-      let startNum = pos
-      while (isNum_(next)) next = input.get(++pos)
-      if (pos == startNum) return
+      input.advance()
+      if (input.next == Plus || input.next == Minus) input.advance()
+      if (!isNum_(input.next)) return
+      do { input.advance() } while (isNum_(input.next))
     }
-    if (next == _f) {
-      if (!/32|64/.test(input.read(pos + 1, pos + 3))) return
-      isFloat = true
-      pos += 3
+    if (input.next == _f) {
+      let after = input.peek(1)
+      if (after == Zero + 3 && input.peek(2) == Zero + 2 ||
+          after == Zero + 6 && input.peek(2) == Zero + 4) {
+        input.advance(3)
+        isFloat = true
+      } else {
+        return
+      }
     }
-    if (isFloat) token.accept(Float, pos)
-  } else if (next == _b || next == _r) {
-    if (next == _b) next = input.get(++pos)
-    if (next != _r) return
-    next = input.get(++pos)
+    if (isFloat) input.acceptToken(Float)
+  } else if (input.next == _b || input.next == _r) {
+    if (input.next == _b) input.advance()
+    if (input.next != _r) return
+    input.advance()
     let count = 0
-    while (next == Hash) { count++; next = input.get(++pos) }
-    if (next != Quote) return
-    next = input.get(++pos)
+    while (input.next == Hash) { count++; input.advance() }
+    if (input.next != Quote) return
+    input.advance()
     content: for (;;) {
-      if (next < 0) return
-      let isQuote = next == Quote
-      next = input.get(++pos)
+      if (input.next < 0) return
+      let isQuote = input.next == Quote
+      input.advance()
       if (isQuote) {
         for (let i = 0; i < count; i++) {
-          if (next != Hash) continue content
-          next = input.get(++pos)    
+          if (input.next != Hash) continue content
+          input.advance()
         }
-        token.accept(RawString, pos)
+        input.acceptToken(RawString)
         return
       }
     }
   }
 })
 
-export const closureParam = new ExternalTokenizer((input, token) => {
-  if (input.get(token.start) == Pipe) token.accept(closureParamDelim, token.start + 1)
+export const closureParam = new ExternalTokenizer(input => {
+  if (input.next == Pipe) input.acceptToken(closureParamDelim, 1)
 })
 
-export const tpDelim = new ExternalTokenizer((input, token) => {
-  let pos = token.start, next = input.get(pos)
-  if (next == LessThan) token.accept(tpOpen, pos + 1)
-  else if (next == GreaterThan) token.accept(tpClose, pos + 1)
+export const tpDelim = new ExternalTokenizer(input => {
+  if (input.next == LessThan) input.acceptToken(tpOpen, 1)
+  else if (input.next == GreaterThan) input.acceptToken(tpClose, 1)
 })
